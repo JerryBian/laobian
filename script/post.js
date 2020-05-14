3538a8a21ebe8b39a9e4f2526d966a81f20f1716
JerryBian <JerryBian@outlook.com>
2020-05-14 13:57:44 +0000

    Deploying to gh-pages from  @ 55979e4a49517a7c7aeab73ed07dafd4261e78a9 🚀

---
+++ script/post.js
@@ -0,0 +1,38 @@
+﻿document.addEventListener("DOMContentLoaded",
+    function() {
+        hljs.initHighlightingOnLoad();
+        anchors.options = {
+            placement: 'right',
+            visible: 'hover'
+        };
+        anchors.add('.post-content h3, .post-content h4');
+
+        try {
+            var modal = document.querySelector("#imgModal");
+            if (!modal) {
+                return;
+            }
+
+            var images = document.querySelectorAll(".post img");
+            var modalImg = document.querySelector("#modal-content");
+            var caption = document.querySelector("#modal-caption");
+            images.forEach(function(img) {
+                img.onclick = function() {
+                    modal.style.display = "block";
+                    modalImg.src = this.src;
+                    caption.innerHTML = this.alt;
+                    document.documentElement.style.overflow = "hidden";
+                    document.body.scroll = "no";
+                };
+            });
+
+            document.querySelector("#modal-close").onclick = function() {
+                modal.style.display = "none";
+                document.documentElement.style.overflow = "auto";
+                document.body.scroll = "yes";
+            };
+        } catch (e) {
+            console.log(e);
+        }
+    },
+    false);
